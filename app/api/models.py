60ab8286138e8e7179796b4aa261758b90751c3e
Lucas Brito <lucasbsilva29@gmail.com>
2025-05-10 20:47:42 -0300

    refactor: rename User class to UserInput and add UserOutput and UserUpdate models

--- app/api/models.py
+++ app/api/models.py
@@ -3,9 +3,20 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 
-class User(BaseModel):
+class UserInput(BaseModel):
     id: str = Field(default=str(uuid4()))
     email: str
     password: str
     created_at: str = Field(default_factory=lambda: datetime.now().isoformat())
     updated_at: str = Field(default_factory=lambda: datetime.now().isoformat())
+
+
+class UserOutput(BaseModel):
+    id: str
+
+
+class UserUpdate(BaseModel):
+    email: str | None = None
+    password: str | None = None
+    created_at: str | None = None
+    updated_at: str | None = None
