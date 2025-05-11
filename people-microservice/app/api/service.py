5debe557269bf3c4da661284a87e9dba13e7a251
Lucas Brito <lucasbsilva29@gmail.com>
2025-05-11 20:59:31 -0300

    feat: change get_user to async for improved performance

--- people-microservice/app/api/service.py
+++ people-microservice/app/api/service.py
@@ -5,7 +5,7 @@ USERS_SERVICE_HOST_URL = 'http://localhost:8000/api/v1/users/'
 url = os.environ.get('USERS_SERVICE_HOST_URL') or USERS_SERVICE_HOST_URL
 
 
-def get_user(user_id: str):
+async def get_user(user_id: str):
     response = httpx.get(f'{url}{user_id}')
     if response.status_code == 200:
         return response.json()
