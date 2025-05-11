a73515b75b0bff5f769bf36146a849e0cf04732a
Lucas Brito <lucasbsilva29@gmail.com>
2025-05-10 21:10:51 -0300

    refactor: streamline user creation and validation logic in users API

--- app/api/users.py
+++ app/api/users.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from typing import List
 from datetime import datetime
 
@@ -5,17 +6,6 @@ from fastapi import APIRouter, HTTPException, Response
 
 from app.api.models import UserInput, UserOutput
 from app.api import db_manager
-
-
-users_db = [
-    {
-        "id": "fc43c30d-61d3-4739-a53d-9ea679c23c91",
-        "email": "example@example.com",
-        "password": "1234",
-        "created_at": "2025-05-10T19:14:03.606753",
-        "updated_at": "2025-05-10T19:14:03.606782",
-    }
-]
 
 users = APIRouter()
 
@@ -25,22 +15,36 @@ async def read_users():
     return await db_manager.get_all_users()
 
 
-@users.post("/", status_code=201)
+@users.post("/", response_model=UserOutput, status_code=201)
 async def create_user(payload: UserInput):
+    payload = payload.model_dump()
+    payload.id = str(uuid4())
     payload.created_at = datetime.now().isoformat()
     payload.updated_at = datetime.now().isoformat()
+
+    validate_user_input(payload)
+    if await db_manager.get_user_by_email(payload.email):
+        raise HTTPException(status_code=400, detail="Email already exists!")
+
     user_id = await db_manager.add_user(payload)
-    response = {'id': user_id, **payload.model_dump()}
-    return response
+
+    return {
+        'id': user_id,
+        **payload.model_dump()
+    }
 
 
 @users.put("/{user_id}")
 async def update_user(user_id: str, payload: UserInput):
-    user = await db_manager.get_user(user_id)
+    user = await db_manager.get_user_by_id(user_id)
     if user:
         update_data = payload.model_dump(exclude_unset=True)
         user_db_input = UserInput(**user)
         user_db_input.model_update(update_data)
+
+        validate_user_input(user_db_input)
+        if user_db_input.id != user_id:
+            raise HTTPException(status_code=400, detail="User ID mismatch!")
 
         payload = user_db_input.model_dump()
         payload.updated_at = datetime.now().isoformat()
@@ -52,9 +56,18 @@ async def update_user(user_id: str, payload: UserInput):
         raise HTTPException(status_code=404, detail="User not found!")
 
 
+def validate_user_input(user_input_data):
+    if not user_input_data.id:
+        raise HTTPException(status_code=400, detail="User ID is required!")
+    if not user_input_data.email:
+        raise HTTPException(status_code=400, detail="Email is required!")
+    if not user_input_data.password:
+        raise HTTPException(status_code=400, detail="Password is required!")
+
+
 @users.delete("/{user_id}", status_code=204)
 async def delete_user(user_id: str):
-    user = await db_manager.get_user(user_id)
+    user = await db_manager.get_user_by_id(user_id)
     if user:
         await db_manager.delete_user(user_id)
         return Response(status_code=204)
