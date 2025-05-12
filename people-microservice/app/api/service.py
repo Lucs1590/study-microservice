e1c9ccfdce9ed79cd4cada3e9ff86bcc5102bcec
Lucas Brito <lucasbsilva29@gmail.com>
2025-05-11 21:23:22 -0300

    fix: update USERS_SERVICE_HOST_URL and add timeout to get_user request

--- people-microservice/app/api/service.py
+++ people-microservice/app/api/service.py
@@ -1,12 +1,12 @@
 import os
 import httpx
 
-USERS_SERVICE_HOST_URL = 'http://localhost:8000/api/v1/users/'
+USERS_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/users/'
 url = os.environ.get('USERS_SERVICE_HOST_URL') or USERS_SERVICE_HOST_URL
 
 
 async def get_user(user_id: str):
-    response = httpx.get(f'{url}{user_id}')
+    response = httpx.get(f'{url}{user_id}', timeout=30)
     if response.status_code == 200:
         return response.json()
     return None
